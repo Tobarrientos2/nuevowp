58c4346092a46564deabbb2c0064a336c94dfd70
Tobarrientos2 <tobarrientos2@gmail.com>
2023-10-06 16:43:22 -0300

    Esperemos

--- wp/wp-config.php
+++ wp/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', 'wrestlingclubconcon' );
 
 /** Database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'mq9hc5zuvbrusuz6s4lm' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', 'pscale_pw_TPt91TTYwIrih3ABSSMs4nbBzqaHWM8XhDy30RaSlMv' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'aws.connect.psdb.cloud' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
